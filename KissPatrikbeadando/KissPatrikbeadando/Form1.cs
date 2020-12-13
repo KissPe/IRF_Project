c35e4c660d2b96160e180ca51198b7ec02edffc0
Patrik Kiss <patrik.kiss9@gmail.com>
2020-12-13 18:00:39 +0100

    2

--- KissPatrikbeadando/KissPatrikbeadando/Form1.cs
+++ KissPatrikbeadando/KissPatrikbeadando/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,29 @@ namespace KissPatrikbeadando
 {
     public partial class Form1 : Form
     {
+        BindingList<Gyumolcslista> gyumolcslista = new BindingList<Gyumolcslista>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            ofd.DefaultExt = "csv";
+            ofd.AddExtension = true;
+
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+            using (StreamReader sr = new StreamReader(ofd.FileName, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split(';');
+                }
+
+
+            }
         }
     }
 }
---
+++ KissPatrikbeadando/KissPatrikbeadando/Gyumolcslista.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KissPatrikbeadando
+{
+    class Gyumolcslista
+    {
+        public int Index { get; set; }
+        public string Név { get; set; }
+        public int Nettóár { get; set; }
+        public int ÁFA { get; set; }
+        public int Összesen { get; set; }
+    }
+}
